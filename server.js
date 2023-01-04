6ceaf578bd518a136a413b8dd45a26a776c41e43
kimnewbie <gracecancho@gmail.com>
2023-01-05 00:08:51 +0900

    Review Part 1

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ MongoClient.connect(
   (에러, client) => {
     if (에러) return console.log(에러);
     //서버띄우는 코드 여기로 옮기기
-    app.listen("8080", function () {
-      console.log("listening on 8080");
+    app.listen("3030", function () {
+      console.log("listening on 3030");
     });
   }
 );
 
-// app.listen(8080, function () {
-//   console.log("listening on 8080.");
+// 내 컴퓨터에서 8080은 이미 사용중이어서 3030으로 변경함
+// app.listen(3030, function () {
+//   console.log("listening on 3030.");
 // });
 
 /* 
